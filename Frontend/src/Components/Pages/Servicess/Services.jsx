4cd1c236877e9feb338fd521f382cc50a616fa54
Khan Tufail <tufailahmedkhan708@gmail.com>
2024-08-08 15:56:11 +0530

    service update

--- Frontend/src/Components/Pages/Servicess/Services.jsx
+++ Frontend/src/Components/Pages/Servicess/Services.jsx
@@ -5,17 +5,17 @@ export default function Services() {
     <div>
         <Link to='/services'>
         </Link>
-        <div className='Flex'>
-          <h1></h1>
+        <p className='mb-5 text-red-500 h-16 text-centre m-auto font-bold text-5xl py-1 border-y-4 bg-yellow-200 text-center'>Library Services</p>
+        <div className="Flex m-8" >
           <div>
+            <h2>Liberary provides the following Services:</h2>
               <ul>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
+                <li><a href="">Membership</a></li>
+                <li><a href="">Book Search &#40;OPAC&#41;	</a></li>
+                <li><a href="">Book issue&#47;renew&#47;reserve</a></li>
+                <li><a href="">Internet facility</a></li>
+                <li><a href="">Photocopy facility</a></li>
+                <li><a href="">Book Bank Scheme &#40;SC&#47;ST&#47;NT&#47;OBC&#41;</a></li>
               </ul>
           </div>
           <div>
