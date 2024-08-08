862f5fa90c95c8c00b3a677aa6cef36951e87210
Khan Tufail <tufailahmedkhan708@gmail.com>
2024-08-08 17:05:36 +0530

    service updated 8/8

--- Frontend/src/Components/Pages/Servicess/Services.jsx
+++ Frontend/src/Components/Pages/Servicess/Services.jsx
@@ -1,22 +1,28 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import services from './assets/services-images.png'
 export default function Services() {
   return (
     <div>
         <Link to='/services'>
         </Link>
-        <p className='mb-5 text-red-500 h-16 text-centre m-auto font-bold text-5xl py-1 border-y-4 bg-yellow-200 text-center'>Library Services</p>
-        <div className="Flex m-8" >
-          <div>
-            <h2>Liberary provides the following Services:</h2>
-              <ul>
-                <li><a href="">Membership</a></li>
-                <li><a href="">Book Search &#40;OPAC&#41;	</a></li>
-                <li><a href="">Book issue&#47;renew&#47;reserve</a></li>
-                <li><a href="">Internet facility</a></li>
-                <li><a href="">Photocopy facility</a></li>
-                <li><a href="">Book Bank Scheme &#40;SC&#47;ST&#47;NT&#47;OBC&#41;</a></li>
+        <p className='text-4xl font-bold mb-8 border-b-4 border-orange-500 pb-4 text-gray-900 text-center'>Library Services</p>
+        <div className="Flex m-8 font-serif" >
+        <h1 className='text-4xl'>Library provides the following Services:</h1>
+          <div className='flex m-8 justify-between '>
+            
+              <ul className='p-0'>
+                <li className='bg-yellow-200 text-3xl flex m-2m justify-between'><p className='m-4'>Membership</p>
+                  <a href=""><FontAwesomeIcon className='mx-4'icon="fa-solid fa-chevron-right" /></a>
+                  </li>
+                <li className='bg-yellow-200 text-3xl flex '><a href="">Book Search &#40;OPAC&#41;<FontAwesomeIcon className='mx-4'icon="fa-solid fa-chevron-right" /></a></li>
+                <li className='bg-yellow-200 text-3xl flex '><a href="">Book issue&#47;renew&#47;<FontAwesomeIcon className='mx-4'icon="fa-solid fa-chevron-right" />reserve</a></li>
+                <li className='bg-yellow-200 text-3xl flex '><a href="">Internet facility<FontAwesomeIcon className='mx-4'icon="fa-solid fa-chevron-right" /></a></li>
+                <li className='bg-yellow-200 text-3xl flex '><a href="">Photocopy facility<FontAwesomeIcon className='mx-4'icon="fa-solid fa-chevron-right" /></a></li>
+                <li className='bg-yellow-200 text-3xl flex '><a href="">Book Bank Scheme &#40;SC&#47;ST&#47;NT&#47;OBC&#41;<FontAwesomeIcon className='mx-4'icon="fa-solid fa-chevron-right" /></a></li>
               </ul>
+              <img src={services} alt="" className=''/>
           </div>
           <div>
 
